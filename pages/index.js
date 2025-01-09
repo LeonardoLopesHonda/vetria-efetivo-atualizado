9e97bc2351eafd31a5761ac59d4a6df89ceb3b00
Leonardo Honda <83879739+LeonardoLopesHonda@users.noreply.github.com>
2025-01-09 22:46:43 +0000

    Moved `api` to `pages` directory

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-  return <h1>Adicione seu arquivo Excel aqui:</h1>;
+  return (
+    <div>
+      <h1>Adicione seu arquivo Excel aqui:</h1>
+      <input type="file" />
+    </div>
+  );
 }
 
 export default Home;
+

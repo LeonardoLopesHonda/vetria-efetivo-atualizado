46e807778621a4926a412cab4a47c9c7511745c2
Leonardo Honda <83879739+LeonardoLopesHonda@users.noreply.github.com>
2025-01-16 18:50:51 +0000

    Expected Field Test

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,19 @@
 function status(request, response) {
-  console.log("TESTing API");
   response.status(200).json({
-    field_1: "Testing 1",
-    field_2: {
-      sub_field: "subfield content são",
+    active_users: {
+      Matricula: "000000001",
+      "Cód. Filial": "1",
+      "Nome Completo do Colaborador": "Leonardo Lopes Honda",
+      "CPF - Número": "12345678912",
+      "Data Nascimento": "26/12/2005",
+      "Data Admissão": "20/05/2024",
+      "Cód. Situação": "1",
+      "Desc. Situação": "Ativo",
+      "Cód. Centro Custo": "00000001",
+      "Denominação do Centro de Custo": "GM - JOVENS APRENDIZES",
+      "Cód. Cargo": "ip872",
+      "Denominação do Cargo": "APRENDIZ EM ADMINISTRACAO",
+      "Data de Rescisão": "",
     },
   });
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,10 @@
 test("GET in /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+
+  console.log(responseBody.active_users);
+  console.log(responseBody.active_users.Matricula);
+  console.log(responseBody.active_users["Nome Completo do Colaborador"]);
 
   expect(response.status).toBe(200);
 });

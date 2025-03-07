067d425592eff30b79f84532755407f3a2e41364
Leonardo Honda <83879739+LeonardoLopesHonda@users.noreply.github.com>
2025-03-07 14:40:41 +0000

    add `concurrently` scripts and refactor `migrations` + `tests`

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,16 @@
 const dotenv = require("dotenv");
 dotenv.config({
-  path: ".env.development",
+  path: ".env.developmet",
 });
 
 const nextJest = require("next/jest");
 const createJestConfig = nextJest({
   dir: ".",
 });
+
 const jestConfig = createJestConfig({
   moduleDirectories: ["node_modules", "<rootDir>"],
+  testTimeout: 60000,
 });
 
 module.exports = jestConfig;
--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,9 +1,10 @@
-import database from "infra/database.js";
+import database from "infra/database";
+import orchestrator from "tests/orchestrator";
 
-beforeAll(clearDatabase);
-async function clearDatabase() {
-  await database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public;");
-}
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await database.query("DROP SCHEMA public cascade; CREATE SCHEMA public;");
+});
 
 test("GET in /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
